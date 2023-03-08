376b00eed204f11809ca9e68d081ad250f3b79f2
Chintan Prajapati <chintan9@users.noreply.github.com>
2023-03-08 22:40:28 +0000

    first commit

---
+++ src/components/ui/RadioGroupBoxStyle.js
@@ -0,0 +1,11 @@
+export const radioGroupBoxstyle = {
+  position: 'absolute',
+  top: '61%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  maxWidth: 500,
+  bgcolor: 'background.paper',
+  border: '2px solid #3f51b5',
+  boxShadow: 24,
+  p: '4px',
+}
